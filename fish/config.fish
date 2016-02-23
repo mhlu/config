16790df9696fce0910f11e6a352e5f07a0a8a7e6
mhlu <mhlu@uwaterloo.ca>
2016-02-22 21:12:41 -0800

    Modify fish alias

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,23 @@
 set -x PIP_REQUIRE_VIRTUALENV true
+
+alias j='command jobs -l'
+
+alias fishrc='command vim ~/.config/fish/config.fish'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+
+alias c='cd'
+alias cp='command cp -i'
+alias mv='command mv -i'
+alias ln='command ln -i'
+
+alias v='command vim'
+alias l='command ls -FG'
+alias la='command ls -FGa'
+alias ll='command ls -FGl'
+
+alias matlab='/Applications/MATLAB_R2015b.app/bin/matlab -nodesktop'
+alias jsc='/System/Library/Frameworks/JavaScriptCore.framework/Versions/Current/Resources/jsc'
+
+alias vim='echo use v'
+alias ls='echo use l'
