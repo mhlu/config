fc26a2d63a4ba70eda4dd4363062c9ff31f2d17c
mhlu <mhlu@uwaterloo.ca>
2016-04-15 11:56:14 -0700

    Prefer brew installed software

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,11 @@
 set -x PIP_REQUIRE_VIRTUALENV true
 set -x GOPATH $HOME/Workspace/go
 set -x PATH $GOPATH/bin $PATH
+set -x PATH /usr/local/bin /usr/local/sbin $PATH
 
 alias ...='cd ../..'
 alias ....='cd ../../..'
 
-alias c='cd'
 alias cp='cp -i'
 alias mv='mv -i'
 alias ln='ln -i'
@@ -16,5 +16,4 @@ alias la='ls -a'
 alias ll='ls -l'
 alias lla='ls -la'
 
-alias matlab='/Applications/MATLAB_R2016a.app/bin/matlab -nodesktop'
 alias jsc='/System/Library/Frameworks/JavaScriptCore.framework/Versions/Current/Resources/jsc'
